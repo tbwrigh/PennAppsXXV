77726de6fe527966a5bcfd5d08a018d448d466dd
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2024-09-21 04:22:26 -0400

    added and designed create meeting page

--- frontend/src/pages/CreateMeeting.tsx
+++ frontend/src/pages/CreateMeeting.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button, Form, Input, Card } from 'antd';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Card, TimePicker, DatePicker } from 'antd';
+import { CalendarOutlined, EditOutlined } from '@ant-design/icons';
 import './CreateMeeting.css';
 
 const CreateMeeting: React.FC = () => {
@@ -12,10 +12,14 @@ const CreateMeeting: React.FC = () => {
     console.log('Failed:', errorInfo);
   };
 
+  const onchange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <div className="wrapper">
       <div className="item">
-        <Card title="Create Meeting" style={{ width: 300 }}>
+        <Card title="Create Meeting" style={{ width: 400, height: 400 }}>
           <Form
             name="create_meeting"
             onFinish={onFinish}
@@ -23,34 +27,32 @@ const CreateMeeting: React.FC = () => {
           >
             <Form.Item
               name="meeting_title"
-              rules={[{ required: true, message: 'Please input the meeting title!' }]}
+              rules={[{ required: true, message: 'Please input the meeting title' }]}
             >
-              <Input prefix={<UserOutlined />} placeholder="Meeting Title" />
+              <Input prefix={<CalendarOutlined />} placeholder="Meeting Title" />  
             </Form.Item>
             <Form.Item
               name="meeting_description"
-              rules={[{ required: true, message: 'Please input the meeting description!' }]}
+              rules={[{ required: true, message: 'Please input the meeting description' }]}
             >
-              <Input prefix={<LockOutlined />} placeholder="Meeting Description" />
+              <Input prefix={<EditOutlined />} placeholder="Meeting Description" />
+            </Form.Item>
+            <Form.Item>
+                <div className="time-picker">
+                <TimePicker.RangePicker format="h:mm a" className="time-picker" />
+                </div>
+            </Form.Item>
+            <Form.Item>
+                <div className="date-picker">
+                <DatePicker multiple onChange={onchange} maxTagCount="responsive" format="MM/DD/YYYY" getPopupContainer={trigger => trigger.parentElement} />
+                </div>
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit">
-                Create Meeting
+                Next
               </Button>
             </Form.Item>
           </Form>
-        </Card>
-      </div>
-      <div className="item">
-        <Card title="Another Item" style={{ width: 300 }}>
-          {/* Add content for the second item here */}
-          <p>This is another item within the wrapper.</p>
-        </Card>
-      </div>
-      <div className="item">
-        <Card title="Third Item" style={{ width: 300 }}>
-          {/* Add content for the second item here */}
-          <p>This is another item within the wrapper.</p>
         </Card>
       </div>
     </div>
